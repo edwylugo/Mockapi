783550ededc95ff0975d31ff8b908c486f70f5dc
Edwy Lugo <edwylugo@gmail.com>
2020-07-03 17:08:55 -0300

    - Implementação Struct Event

---
+++ MockApi/API/MockREST.swift
@@ -0,0 +1,9 @@
+//
+//  MockREST.swift
+//  MockApi
+//
+//  Created by Edwy Lugo on 03/07/20.
+//  Copyright © 2020 Edwy Lugo. All rights reserved.
+//
+
+import Foundation
---
+++ MockApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockApi
+//
+//  Created by Edwy Lugo on 03/07/20.
+//  Copyright © 2020 Edwy Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
