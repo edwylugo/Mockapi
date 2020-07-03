22524144f10aba5436acfc3903772b7c711978bf
Edwy Lugo <edwylugo@gmail.com>
2020-07-03 18:35:24 -0300

    - Include viewModel Coordinator - Include Class ListTableViewCell - Include xib ListTableViewCell - Implements viewModelProtocol ListEvents

---
+++ MockApi/Scenes/ListEvents/Cell/ListEventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListEventTableViewCell.swift
+//  MockApi
+//
+//  Created by Edwy Lugo on 03/07/20.
+//  Copyright © 2020 Edwy Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ListEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
